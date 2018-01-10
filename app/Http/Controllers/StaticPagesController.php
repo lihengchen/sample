6d7156672708c91ccda8d1b4fbfea5b0267a9d3c
luke2017 <l1270318090@gmail.com>
2018-01-10 02:40:25 +0000

    Finish static pages

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    //
+ public function home()
+    {
+    	//$a='hello555dddd';
+    	//return $a;
+        return view('static_pages.home');
+    }
+
+    public function help()
+    {
+        return view('static_pages/help');
+    }
+
+    public function about()
+    {
+        return view('static_pages/about');
+}
+}
