cf5e35d53de0450efd272b8a76e86399d369f0cf
luke2017 <l1270318090@gmail.com>
2018-01-10 03:42:16 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,13 @@
 <html>
   <head>
     <title>@yield('title','sample')--学习demo</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
+   @include('layouts._header')
+    <div class="container">
     @yield('content')
+     @include('layouts._footer')
+    </div>
   </body>
 </html>
